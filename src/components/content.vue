6711ae36342034e237013d3683675b6745e41b4c
Mou <madalmou.2004@gmail.com>
2024-01-02 08:48:14 +0530

    first commit

---
+++ src/components/content.vue
@@ -0,0 +1,107 @@
+<script setup>
+await new Promise((res) => setTimeout(res, 2000));
+
+</script>
+<template>
+    <div class="container">
+        <h1>2024 <img src="../assets/image.jpg" alt=""> </h1>
+
+        <h3>A New Year is like a blank book, and the pen is in your hands. It is your chance to write a beautiful story for
+            yourself. 
+            Happy New Year.
+
+            "Tomorrow, is the first blank page of a 365-page book. Write a good one!" — Brad Paisley
+            New is the year, new are the hopes, new is the resolution, new are the spirits, and new are my warm wishes just
+            for you. Have a promising and fulfilling New Year!</h3>
+
+    </div>
+</template>
+<style>
+body {
+
+
+    background-image: url("../assets/whether.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+    background-size: cover;
+    text-align: center;
+
+}
+
+
+
+.container {
+
+
+    height: 55vh;
+    width: 20vw;
+    text-align: center;
+    box-sizing: border-box;
+    box-shadow: 10px 10px 10px 10px black;
+    border-radius: 30px;
+    display: block;
+    background: transparent;
+    box-shadow: 0 0 5px #2db2ff, 0 0 15px #2db2ff, 0 0 30px #2db2ff,
+        0 0 60px #2db2ff;
+    transition: all 2s;
+}
+
+.container:hover {
+    background-color: rgba(0, 0, 0, 0.872);
+    box-shadow: 0 0 5px #2db2ff, 0 0 15px #2db2ff, 0 0 30px #2db2ff,
+        0 0 60px #2db2ff;
+}
+
+h1 {
+
+    font-weight: 500;
+    color: #2db2ff;
+    text-shadow: 12px 12px 14px #000000;
+    cursor: pointer;
+    font-size: 80px;
+    font-family:cursive;
+}
+
+
+
+img {
+    width: 80px;
+    border-radius: 200px;
+
+}
+
+
+h3 {
+    font-size:large;
+    color: #2db2ff;
+    text-shadow: 2px 2px 4px #000000;
+    font-family: cursive;
+    cursor: pointer;
+    text-transform:capitalize;
+
+}
+
+
+
+
+
+
+div.btn {
+   display: none;
+}
+
+@media only screen and (max-width:610px) {
+    .container {
+        min-height: 60vh;
+        min-width: 100vw;
+    }
+}
+
+@media only screen and (min-width:620px) {
+    .container {
+        min-height: 25vh;
+        min-width: 48vw;
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,165 @@
+<template>
+    <Suspense v-if="show">
+        <content />
+        <template #fallback>
+            <!-- <P>Loading.....</P> -->
+        </template>
+    </Suspense>
+
+    <div class="btn" @click="show = true"><a href="#">2024</a></div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { defineAsyncComponent } from 'vue';
+const content = defineAsyncComponent({
+    loader: () => import("../components/content.vue"),/*our component loader*/
+});
+const show = ref(false);
+
+</script>
+
+
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+}
+
+
+body {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    min-width: 100vw;
+    background: linear-gradient(to bottom, #5d326c, #350048);
+    
+}
+
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.btn {
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 200px;
+    height: 45px;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.btn a {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(255, 255, 255, 0.05);
+    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.3);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+    border-top: 1px solid rgba(255, 255, 255, 0.1);
+    border-radius: 30px;
+    padding: 10px;
+    letter-spacing: 1px;
+    text-decoration: none;
+    overflow: hidden;
+    color: #fff;
+    font-weight: 400px;
+    z-index: 1;
+    transition: 0.5s;
+    backdrop-filter: blur(15px);
+}
+
+.btn:hover a {
+    letter-spacing: 12px;
+    font-family: cursive;
+
+}
+
+.btn a::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    background: linear-gradient(to left, rgba(255, 255, 255, 0.15), transparent);
+    transform: skewX(45deg) translate(0);
+    transition: 0.5s;
+    filter: blur(0px);
+}
+
+.btn:hover a::before {
+    transform: skewX(45deg) translate(200px);
+}
+
+.btn::before {
+    content: "";
+    position: absolute;
+    left: 50%;
+    transform: translatex(-50%);
+    bottom: -5px;
+    width: 30px;
+    height: 10px;
+    background: #f00;
+    border-radius: 10px;
+    transition: 0.5s;
+    transition-delay: 0.5;
+}
+
+.btn:hover::before
+
+/*lightup button*/
+    {
+    bottom: 0;
+    height: 50%;
+    width: 80%;
+    border-radius: 30px;
+}
+
+.btn::after {
+    content: "";
+    position: absolute;
+    left: 50%;
+    transform: translatex(-50%);
+    top: -5px;
+    width: 30px;
+    height: 10px;
+    background: #f00;
+    border-radius: 10px;
+    transition: 0.5s;
+    transition-delay: 0.5;
+}
+
+.btn:hover::after
+
+/*lightup button*/
+    {
+    top: 0;
+    height: 50%;
+    width: 80%;
+    border-radius: 30px;
+}
+
+.btn:nth-child(1)::before,
+/*chnage 1*/
+.btn:nth-child(1)::after {
+    background: #ff1f71;
+    box-shadow: 0 0 5px #ff1f71, 0 0 15px #ff1f71, 0 0 30px #ff1f71,
+        0 0 60px #ff1f71;
+}
+</style>
+
